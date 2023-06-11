6d733815a94043d79a538660472a397496c930c7
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-06-11 19:22:14 +0300

    2-nd task

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,2 +1,23 @@
-import vimeo from '@vimeo/player';
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+player.on('play', function() {
+  console.log('played the video!');
+});
+
+player.on('timeupdate', throttle(function(timeupdate) {
+  localStorage.setItem("videoplayer-current-time", timeupdate.seconds);
+}, 1000));
+
+const savedTime = localStorage.getItem("videoplayer-current-time");
+
+console.log(savedTime);
+
+if (savedTime) {
+  player.setCurrentTime(parseFloat(savedTime)).catch(function(error) {
+    console.error('Помилка при встановленні часу: ' + error);
+  });
+}
