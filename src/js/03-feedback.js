41e0d9d9f0d117705e201b72db9ee14a6bb5e8e1
Yaroslav Grabovych <ygrabovych@gmail.com>
2023-06-11 22:14:45 +0300

    3-d task

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,38 @@
+import throttle from 'lodash.throttle';
 
+const STORAGE_KEY = 'userData';
+
+const refs = {
+    form: document.querySelector("form"),
+    inputEmail: document.querySelector("input"),
+    inputTextarea: document.querySelector("textarea"),
+};
+
+refs.form.addEventListener('submit', onSubmitForm);
+refs.form.addEventListener('input', throttle(onInputArea, 1250));
+
+const formData = {};
+
+function onInputArea(evt) {
+
+    formData[evt.target.name] = evt.target.value;
+
+    localStorage.setItem(STORAGE_KEY, JSON.stringify(formData));
+};
+
+function onSubmitForm(evt) {
+evt.preventDefault();
+evt.currentTarget.reset();
+localStorage.clear();
+};
+
+function restorePage (){
+    const savedData = JSON.parse(localStorage.getItem(STORAGE_KEY));
+
+if(savedData) {
+    refs.inputTextarea.value = savedData.message || "";
+    refs.inputEmail.value = savedData.email || "";
+};
+};
+
+document.addEventListener('DOMContentLoaded', restorePage);
